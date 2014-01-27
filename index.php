96951d09561584a1512e8d1b397bf5575cdc76e9
Abhi <abhijeetgoel77@gmail.com>
2014-01-27 20:29:59 +0530

    changes

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Get rich - every hour</title>
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta charset="UTF-8">
+<!--<link rel="stylesheet" type="text/css" href="css/bootstrap.css"></link>-->
+<link rel="stylesheet" type="text/css" href="css/style.css"></link>
+</head>
+<body>
+	<div id="content">
+		Hello, how are you!!
+	</div>
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/game.js"></script>
+</body>
+</html>
